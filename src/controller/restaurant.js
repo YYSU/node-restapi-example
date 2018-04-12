7c6048186543a7109ed1097db2fe4cfb66722a7d
彥昀 蘇 <jim123820@yahoo.com.tw>
2018-04-12 17:52:55 +0800

    Add restaurant put

--- src/controller/restaurant.js
+++ src/controller/restaurant.js
@@ -26,19 +26,35 @@ export default ({ config, db }) => {
             if (err) {
                 res.send(err);
             }
-            res.json({ restaurants, message: 'Restaurant got successfully'})
-        })
-    })
+            res.json({ restaurants, message: 'Restaurant got successfully' });
+        });
+    });
 
-    // '/v1/restaurant/:id' - Read
+    // '/v1/restaurant/:id' - Read 1
     api.get('/:id', (req, res) => {
         Restaurant.findById(req.params.id, (err, restaurant) => {
             if (err) {
                 res.send(err);
             }
-            res.json({ restaurant, message: 'Restaurant got successfully'})
-        })
-    })
+            res.json({ restaurant, message: 'Restaurant got successfully' });
+        });
+    });
+
+    // '/v1/restaurant/:id' - Update
+    api.put('/:id', (req, res) => {
+        Restaurant.findById(req.params.id, (err, restaurant) => {
+            if (err) {
+                res.send(err);
+            }
+            restaurant.name = req.body.name;
+            restaurant.save(err => {
+                if (err) {
+                    res.send(err);
+                }
+                res.json({ message: "Restaurant info updated" });
+            });
+        });
+    });
 
     return api;
 }
